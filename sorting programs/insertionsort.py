c377951861c68c761143182829c1f5d601286880
PRAVEEN JAYAPRAKASH <praveenpilot6e452@gmail.com>
2024-05-23 16:25:31 +0530

    insertion sort

--- sorting programs/insertionsort.py
+++ sorting programs/insertionsort.py
@@ -7,11 +7,11 @@ def insertionsort(arr):
                 a=arr[j]
                 arr[j]=arr[j+1]
                 arr[j+1]=a
-                for k in range(b,1,-1):
+                for k in range(b,0,-1):
                     if arr[k]<arr[k-1]:
                         c=arr[k]
                         arr[k]=arr[k-1]
                         arr[k-1]=c
     print(arr)
-data=[5,3,2,4,1,10,6,7]
+data=[5,3,2,4,1,10,6,7,0,3,5,1,6,0,2,4,6]
 insertionsort(data)
