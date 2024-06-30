4c2913529f9a187a912c003fa1c0656d0347932e
PRAVEEN JAYAPRAKASH <praveenpilot6e452@gmail.com>
2024-06-30 09:36:36 +0530

    problems

--- leetcode problems/kth largest element.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def findKthLargest(nums, k):
-        a=tuple(sorted(nums,reverse=True))
-        return a[k-1]
-    nums=[3,2,1,5,6,4]
-    k=2
-    print(findKthLargest(nums,k))
-#arrenge in decending order and returmn k-1 th term for kth largest , arrange in ascending order and
-#return k-1 term for kth smallest 
